ca4614b03448c5f050ff7c98ced166837ebd42b5
shrinalpatel <91128639+shrinalpatel@users.noreply.github.com>
2021-12-12 20:27:24 -0500

    Update ViewController.swift
    Updated Swipe Gestures

--- TodoList/TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/TodoList/ViewController.swift
@@ -8,7 +8,7 @@
 //  Author: Vidhu Gaba
 //  Student id: 301210694
 //
-//  Date: 27 November 2021
+//  Date: 12 December 2021
 //  App name: TodoList
 //  App Description: A simple todo list iOS application
 
@@ -16,10 +16,13 @@ import UIKit
 import CoreData
 
 var todoList = [Todo]()
+var todos: [NSManagedObject] = []
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var TodoTableView: UITableView!
+    @IBOutlet weak var mySwitch: UISwitch!
     
     var firstLoad = true
     var selectedTodo: Todo? = nil
@@ -70,6 +73,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        TodoTableView.reloadData()
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+              return
+          }
+          
+          let managedContext =
+            appDelegate.persistentContainer.viewContext
+          
+          let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Todo")
+          
+          do {
+            todos = try managedContext.fetch(fetchRequest)
+          } catch let error as NSError {
+            print("Could not fetch todos. \(error), \(error.userInfo)")
+          }
+    }
+
+    
     // Function for number of rows in table view
     
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -86,13 +111,114 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         thisTodo = nonDeletedTodos()[indexPath.row]
         
         cell.todoName.text = thisTodo.name
-        cell.todoStatus.text = thisTodo.desc
-        cell.editButton.tag = indexPath.row
+        cell.todoStatus.text = "Incomplete"
         
         return cell
     
     }
     
+    // Function for swiping left and deleting a todo
+    
+    private func deleteTodo(indexPath: IndexPath) {
+        TodoTableView.beginUpdates()
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+            return
+          }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        managedContext.delete(todos[indexPath.row])
+        do {
+            try managedContext.save()
+            todos.remove(at: indexPath.row)
+        } catch let error as NSError {
+            print("Could not delete. \(error), \(error.userInfo)")
+        }
+        TodoTableView.deleteRows(at: [indexPath], with: .fade)
+        TodoTableView.endUpdates()    }
+    
+    //Function for swiping left and marking a todo completed
+    
+    private func completeTodo(indexPath: IndexPath) {
+        print("complete")
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+            return
+          }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let isCompleted = todos[indexPath.row].value(forKey: "isCompleted") as! Bool
+        todos[indexPath.row].setValue(!isCompleted, forKey: "isCompleted")
+    
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not update. \(error), \(error.userInfo)")
+        }
+        print("Todo updated")
+        TodoTableView.reloadData()
+        
+    }
+    
+    //Swipe right function for editing a todo
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
+    {
+        
+        let editTodoSwipe = UIContextualAction(style: .normal, title: "Edit")
+        {
+            action, view, edit in
+            
+            let editTodoDetails = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "editTodo") as! DetailsViewController
+    
+            let transition:CATransition = CATransition()
+                    transition.duration = 0.5
+                    transition.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+                    transition.type = .push
+                    transition.subtype = .fromLeft
+
+            self.navigationController?.view.layer.add(transition, forKey: kCATransition)
+            
+            self.navigationController?.pushViewController(editTodoDetails, animated: true)
+        }
+
+        editTodoSwipe.backgroundColor = .systemBlue
+        return UISwipeActionsConfiguration(actions: [editTodoSwipe])
+    }
+    
+    //Swipe left function for deleting and marking a todo completed
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let completeTodoSwipe = UIContextualAction(style: .normal,
+                                       title: "Complete")
+        { [weak self] (action, view, completionHandler) in
+            self?.completeTodo(indexPath: indexPath)
+        completionHandler(true)
+        }
+
+
+        let deleteTodoSwipe = UIContextualAction(style: .destructive, title: "Delete") {
+            [weak self] (action, view, completionHandler) in
+            self?.deleteTodo(indexPath: indexPath)
+            completionHandler(true)
+            
+            /*let point = (sender as AnyObject).convert(CGPoint.zero, to: self?.TodoTableView)
+            //guard let indexpath = self?.TodoTableView.indexPathForRow(at: point) else {return}
+            
+            self?.TodoTableView.beginUpdates()
+            todoList.remove(at: indexPath.row)
+            self?.TodoTableView.reloadData()
+            self?.TodoTableView.deleteRows(at: [IndexPath(row: 2, section: 1)], with: .fade)
+            self?.TodoTableView.endUpdates()*/
+            
+            
+        }
+    
+        completeTodoSwipe.backgroundColor = .systemYellow
+        return UISwipeActionsConfiguration(actions: [deleteTodoSwipe, completeTodoSwipe])
+        
+        }
+   
     override func viewDidAppear(_ animated: Bool)
         {
             TodoTableView.reloadData()
@@ -133,4 +259,5 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 
 }
+
 
